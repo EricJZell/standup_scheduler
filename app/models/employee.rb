0a2aa945be8eaaa789990b6ea752953eb456a453
Eric Zell <EricJZell@gmail.com>
2015-10-27 17:48:05 -0400

    can now schedule standups

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ActiveRecord::Base
   validates :name, presence: true
   validates :active, presence: true
-  has_many :employee_standups
-  has_many :standups, through: :employee_standups
+  has_many :employee_standups, dependent: :destroy
+  has_many :standups, through: :employee_standups, dependent: :destroy
 end
